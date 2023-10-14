da2033b01aba0b4e73e895b16736e61e6e729749
Eder Rosa <ederrosa@Eders-MacBook-Air.local>
2023-10-14 12:15:40 -0300

    feat(spinner video)

--- src/app/(portfolio)/portfolio/page.tsx
+++ src/app/(portfolio)/portfolio/page.tsx
@@ -5,7 +5,7 @@ export default function Portfolio() {
     <main className="m-auto min-h-screen max-w-[1440px] bg-[#F6F6F6]">
       <section className="mx-auto flex max-w-[1400px] flex-col overflow-hidden">
         <div className="mt-16 flex w-full items-center justify-center overflow-hidden ">
-          <h2 className="w-full break-keep text-center text-[19.5vw] font-extralight tracking-tighter">
+          <h2 className="w-[100vw] break-keep bg-red-200 text-center text-[19.5vw] font-extralight tracking-tighter">
             Tony Özkan
           </h2>
         </div>
--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,6 +1,6 @@
 'use client'
+import { Loader2 } from 'lucide-react'
 import { useEffect, useState } from 'react'
-import ReactPlayer from 'react-player'
 
 export default function VideoPlayer() {
   const [isVideoPlayerMounted, setIsVideoPlayerMounted] = useState(false)
@@ -12,24 +12,26 @@ export default function VideoPlayer() {
   return (
     <>
       <div className="flex h-screen items-center justify-center">
-        {isVideoPlayerMounted && (
-          // USE LATER:
-          // <ReactPlayer
-          //   style={{
-          //     backgroundColor: 'black',
-          //   }}
-          //   width={'100%'}
-          //   url="https://giistyxelor.s3.amazonaws.com/giists/video/video0cP3w019TiZYYcUy22WY.mp4"
-          //   controls={false}
-          //   playing={true}
-          //   muted
-          //   loop
-          // />
+        {isVideoPlayerMounted ? (
           <video autoPlay muted loop style={{ width: '100%', height: '500px' }}>
             <source src="https://giistyxelor.s3.amazonaws.com/giists/video/video0cP3w019TiZYYcUy22WY.mp4" />
           </video>
+        ) : (
+          <Loader2 className="animate-spin" />
         )}
       </div>
     </>
   )
 }
+
+// <ReactPlayer
+// style={{
+//   backgroundColor: 'black',
+// }}
+// width={'100%'}
+// url="https://giistyxelor.s3.amazonaws.com/giists/video/video0cP3w019TiZYYcUy22WY.mp4"
+// controls={false}
+// playing={true}
+// muted
+// loop
+// />
