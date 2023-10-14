67047023841e88789f3116829e10e43c28fb6899
Eder Rosa <ederrosa@Eders-MacBook-Air.local>
2023-10-14 14:23:59 -0300

    fix(window size)

--- src/components/custom-cursor.tsx
+++ src/components/custom-cursor.tsx
@@ -1,12 +1,10 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 
 const Cursor: React.FC<{ cursorVariant: string }> = ({ cursorVariant }) => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 1000 })
-
-  const windowSize = useRef([window.innerWidth, window.innerHeight])
 
   useEffect(() => {
     const handleMouseMove = (e: MouseEvent) => {
@@ -37,13 +35,11 @@ const Cursor: React.FC<{ cursorVariant: string }> = ({ cursorVariant }) => {
 
   return (
     <>
-      {windowSize.current[0] > 768 && (
-        <motion.div
-          className="pointer-events-none fixed left-0 top-[1000px] z-50 h-5 w-5 rounded-full bg-slate-800"
-          variants={variants}
-          animate={cursorVariant}
-        ></motion.div>
-      )}
+      <motion.div
+        className="pointer-events-none fixed left-0 top-[1000px] z-50 hidden h-5 w-5 rounded-full bg-slate-800 sm:block"
+        variants={variants}
+        animate={cursorVariant}
+      ></motion.div>
     </>
   )
 }
